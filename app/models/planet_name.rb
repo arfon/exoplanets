b8eb40f9d65bdfefe485752c8acd203ba863b6b3
Arfon Smith <arfon.smith@gmail.com>
2013-04-13 20:46:03 -0500

    Desc

--- app/models/planet_name.rb
+++ app/models/planet_name.rb
@@ -15,7 +15,7 @@ class PlanetName < ActiveRecord::Base
   end
   
   def self.trending
-    limit(10).order(:vote_count)
+    limit(10).order('vote_count DESC')
   end
   
   state_machine :status, :initial => :active do
